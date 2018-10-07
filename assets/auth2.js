716ed78f5c0dad626b8ded9b049a13171e2d4eac
himanshu khosla <himanshukhosla123@gmail.com>
2018-10-08 01:47:34 +0530

    auto commit

--- assets/auth2.js
+++ assets/auth2.js
@@ -2,7 +2,7 @@ var PROJECT_ID = 'anu-bknhvv';
      var CLIENT_ID = '727213410009-7s91hsq2089f656b19iik147apeplq9q.apps.googleusercontent.com';
      var API_KEY = 'AIzaSyA5E0XSqyaQsoj2IuAjDIGDbhZCKL5Atqw';
      var SCOPES = 'https://www.googleapis.com/auth/cloud-platform';
-
+var access_token="";
 function authorization() {
        gapi.client.setApiKey(API_KEY);
        gapi.auth.authorize({
@@ -11,10 +11,11 @@ function authorization() {
          immediate: false
        }, function(authResult) {
             if (authResult && !authResult.error) {
-              window.alert('Auth was successful!');
+//              window.alert('Auth was successful!');
                 console.log(authResult);
+                access_token=authResult.access_token;
             } else {
-              window.alert('Auth was not successful');
+//              window.alert('Auth was not successful');
             }
           }
        );
