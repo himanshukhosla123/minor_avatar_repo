d33a87f48cdd540079f812edac3906d1de57b019
himanshu khosla <himanshukhosla123@gmail.com>
2018-10-07 23:48:55 +0530

    aa

---
+++ assets/auth.js
@@ -0,0 +1,45 @@
+var access_token="";
+
+function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+}
+
+      function initClient() {
+        gapi.client.init({
+            apiKey: 'AIzaSyA5E0XSqyaQsoj2IuAjDIGDbhZCKL5Atqw',
+//            discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+            clientId: '727213410009-7s91hsq2089f656b19iik147apeplq9q.apps.googleusercontent.com',
+            scope: 'profile'
+        }).then(function () {
+          gapi.auth2.getAuthInstance()
+              .isSignedIn
+              .listen(updateSigninStatus);
+            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        });
+      }
+
+      function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+          makeApiCall();
+        }
+      }
+
+      function handleSignInClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+
+      function handleSignOutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+
+      function makeApiCall() {
+        // Make an API call to the People API, and print the user's given name.
+        gapi.client.people.people.get({
+          'resourceName': 'people/me',
+          'requestMask.includeField': 'person.names'
+        }).then(function(response) {
+          console.log('Hello, ' + response.result.names[0].givenName);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });
+      }
